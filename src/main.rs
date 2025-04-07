c9533a48d42fed7772f0e571db6540e753be4b9b
Jash Mevada <98532059+jashmevada@users.noreply.github.com>
2025-04-07 10:43:18 +0530

    added code.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::fs::{OpenOptions, metadata};
+use std::process;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 2 {
+        let file_path = &args[1];
+
+        match metadata(file_path) {
+            Ok(_) => println!("The file already exists. No changes made."),
+            Err(_) => {
+                match OpenOptions::new().write(true).create_new(true).open(file_path) {
+                    Ok(_) => println!("The file is created successfully."),
+                    Err(_) => {
+                        println!("The file could not be opened or created. The program will exit now.");
+                        process::exit(0);
+                    }
+                }
+            }
+        }
+    } else {
+        println!("Usage: touch <file_path>");
+    }
 }
